65958d58317d3a458142f7b9def535185eff1f7e
nk87526 <102371208+nk87526@users.noreply.github.com>
2023-07-24 22:32:25 -0500

    Add files via upload
    " My First commit, excited!"

---
+++ jult241630/src/main/java/org/example/com/memberenrollment/controller/MemberController.java
@@ -0,0 +1,29 @@
+package org.example.com.memberenrollment.controller;
+
+import org.example.com.memberenrollment.Member;
+import org.example.com.memberenrollment.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @PostMapping("/members")
+    public String createMember(@RequestBody Member member) {
+        // Validate input data if needed
+        // ...
+
+        // Save the member to the database
+        memberRepository.save(member);
+
+        return "Member enrollment successful!";
+    }
+}
